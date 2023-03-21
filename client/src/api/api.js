df412fe2702dab66613b9c870038bcb1796555df
yoonae7 <pianodrawing@gmail.com>
2023-03-21 16:25:34 +0900

    커뮤니티 추가

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -31,3 +31,10 @@ export const getReview = () => {
     .then(res => res.data)
     .catch(err => console.log(err));
 };
+
+export const getCommunity = () => {
+  return axios
+    .get('http://localhost:3001/community')
+    .then(res => res.data)
+    .catch(err => console.log(err));
+};
