bb493fd9fa1f6de58164c0281212216a9841d00c
ofcpire <ofcpire@gmail.com>
2023-03-24 10:13:37 +0900

    타입 전역화

---
+++ client/src/@types/index.d.ts
@@ -0,0 +1,19 @@
+interface ArticleType {
+  id:number;
+  member_id:number;
+  title:string;
+  content:string;
+  nickName:string;
+  articleImg:string;
+  profileImg:string;
+  carName:string;
+  isLiked:boolean;
+  viewCnt:number;
+  commentCnt:number;
+  likeCnt:number;
+  createdAt:string;
+  updatedAt:string;
+}
+
+
+
