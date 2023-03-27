1951cc860bd77ef995a15a69f46553c07683b363
yoonae7 <pianodrawing@gmail.com>
2023-03-27 10:09:07 +0900

    헤더 로그인 권한설정

--- client/src/store/memberInfoSlice.tsx
+++ client/src/store/memberInfoSlice.tsx
@@ -1,19 +1,27 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState:MemberInfo|null=null;
+let initialState: MemberInfo = {
+  id: 0,
+  email: '',
+  nickname: '',
+  profileImg: '',
+  about: '',
+  carName: '',
+  oilInfo: '',
+};
 
 const memberInfoSlice = createSlice({
   name: 'memberInfo',
   initialState,
   reducers: {
     setMemberInfo: (state, action) => {
-      return state=action.payload;
+      return (state = action.payload);
     },
-    removeMemberInfo: (state) => {
-      return state=null;
+    removeMemberInfo: state => {
+      return (state = initialState);
     },
   },
 });
 
-export const { setMemberInfo,removeMemberInfo } = memberInfoSlice.actions;
+export const { setMemberInfo, removeMemberInfo } = memberInfoSlice.actions;
 export default memberInfoSlice;
