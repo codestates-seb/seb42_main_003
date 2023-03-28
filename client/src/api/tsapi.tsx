d66551c9c16b543398a2ca803005666480fcdcf9
ofcpire <ofcpire@gmail.com>
2023-03-28 09:48:12 +0900

    api 함수 promise 성공 실패 반환

--- client/src/api/tsapi.tsx
+++ client/src/api/tsapi.tsx
@@ -4,69 +4,82 @@ import { loadAccessToken, loadRefreshToken } from '../utils/token';
 const url = process.env.REACT_APP_API_URL;
 
 //데이터를 받아올 때 사용하는 함수, params매개변수에 파라미터를 객체로 전달
-export const getDataTs = (endpoint: string, params: {} = {}) => {
-  return axios({
-    method: 'get',
-    url: `${url}/${endpoint}`,
-    params: params,
-  })
-    .then(res => res.data)
-    .catch(err => err);
+export const getDataTs = async (endpoint: string, params: {} = {}) => {
+  try {
+    const res = await axios({
+      method: 'get',
+      url: `${url}/${endpoint}`,
+      params: params,
+    });
+    return Promise.resolve(res.data);
+  } catch (err) {
+    return Promise.reject(err);
+  }
 };
 
 //이미지가 없는 데이터를 보낼 때 사용하는 함수
-export const sendDataTs = (endpoint: string, method: string, data: object) => {
-  return axios({
-    method: method,
-    url: `${url}/${endpoint}`,
-    data: data,
-    headers: {
-      Authorization: loadAccessToken(),
-      Refresh: loadRefreshToken(),
-    },
-  })
-    .then(res => res.data)
-    .catch(err => err);
+export const sendDataTs = async (
+  endpoint: string,
+  method: string,
+  data: object
+) => {
+  try {
+    const res = await axios({
+      method: method,
+      url: `${url}/${endpoint}`,
+      data: data,
+      headers: {
+        Authorization: loadAccessToken(),
+        Refresh: loadRefreshToken(),
+      },
+    });
+    return Promise.resolve(res.data);
+  } catch (err) {
+    return Promise.reject(err);
+  }
 };
 
 //이미지가 있는 데이터를 보낼 때 사용하는 함수
-export const sendFormDataTs = (
+export const sendFormDataTs = async (
   endpoint: string,
   method: string,
   data: object,
-  image: FileList | undefined
+  image: FileList | undefined,
+  key:string
 ) => {
   let formData = new FormData();
-  formData.append('data', JSON.stringify(data));
+  formData.append(key, JSON.stringify(data));
   if (image) formData.append('image', image[0]);
-  return axios({
-    method,
-    url: `${url}/${endpoint}`,
-    data: formData,
-    headers: {
-      Authorization: loadAccessToken(),
-      Refresh: loadRefreshToken(),
-      'Content-Type': 'multipart/form-data',
-    },
-  })
-    .then(res => res.data)
-    .catch(err => err);
+  try {
+    const res = await axios({
+      method,
+      url: `${url}/${endpoint}`,
+      data: formData,
+      headers: {
+        Authorization: loadAccessToken(),
+        Refresh: loadRefreshToken(),
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+    return Promise.resolve(res.data);
+  } catch (err) {
+    return Promise.reject(err);
+  }
 };
 
-export const loginTs = (data: {}) => {
-  return axios({
-    method: 'post',
-    url: `${url}/members/login`,
-    data: data,
-    headers: {
-      Authorization: loadAccessToken(),
-      Refresh: loadRefreshToken(),
-    },
-  })
-    .then(res => {
-      sessionStorage.setItem('authorization', res.headers.authorization);
-      localStorage.setItem('refresh', res.headers.refresh);
-      return res.data;
-    })
-    .catch(err => err);
+export const loginTs = async (data: {}) => {
+  try {
+    const res = await axios({
+      method: 'post',
+      url: `${url}/members/login`,
+      data: data,
+      headers: {
+        Authorization: loadAccessToken(),
+        Refresh: loadRefreshToken(),
+      },
+    });
+    return Promise.resolve(res.data);
+  } catch (err) {
+    return Promise.reject(err);
+  }
 };
